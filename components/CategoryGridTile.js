5e634cc0458f8362aa44894f9c5bac8cb7a23104
ergincetn01 <ergincetn01@gmail.com>
2023-09-13 00:04:10 +0300

    custom grid tiles impl. dummy data added

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,54 @@
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.btn,
+          pressed ? styles.btnpressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    flex: 1,
+  },
+  btnpressed: {
+    opacity: 0.5,
+  },
+  gridItem: {
+    flex: 1,
+    height: 160,
+    width: 160,
+    margin: 6,
+    borderRadius: 12,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    elevation: 8,
+    shadowColor: "black",
+    shadowOpacity: 1,
+    backgroundColor: "white",
+    shadowRadius: 12,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default CategoryGridTile;
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,19 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy_data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+    return <CategoryGridTile title={itemData.item.title}  color={itemData.item.color} />; 
+}
+
+function CategoriesScreen() {
+    return ( 
+        <FlatList data={CATEGORIES}
+        keyExtractor={(item)=> item.id}
+        renderItem={(renderCategoryItem)}
+        numColumns="2"
+        />
+     );
+}
+
+export default CategoriesScreen;
